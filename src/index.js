cf87a8d2f30fb1667d6613b63e2818c0560bc3a3
Ben Gray <bendoylegray@gmail.com>
2019-06-05 19:45:51 -0400

    removes unneeded files

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
 const store = configureStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
